44186014ffb6c5bde30f90c7257be63187c5693d
aevilesaguiar <aeviles@hotmail.com>
2022-04-29 18:13:20 -0300

    subindo arquivo para a azure

--- src/main/java/com/aeviles/agenda/config/SecurityConfig.java
+++ src/main/java/com/aeviles/agenda/config/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.aeviles.agenda.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +41,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             // other public endpoints of your API may be appended to this array
             "/login"
     };
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/v2/api-docs",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/configuration/ui",
+                "/configuration/security",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/v3/api-docs/**",
+                "/login",
+                "/swagger-ui/**",
+                "/webjars/**");
+    }
+
 
 
     //metodoS gerado da própria  classe SecurityConfig  alt+insert -override
--- src/main/java/com/aeviles/agenda/config/SwaggerConfig.java
+++ src/main/java/com/aeviles/agenda/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.aeviles.agenda.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,6 +12,8 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 
 @Configuration
 @EnableSwagger2
@@ -20,7 +24,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.aeviles.agenda.api"))
+                .apis( RequestHandlerSelectors.basePackage("com.aeviles.agenda.api" ))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
@@ -29,9 +33,9 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot REST API")
-                .description("Aplicação de Agendamento de Consultas")
-                .version("1.0.0")
-                .contact(new Contact("Aeviles Aguiar", "http://github.com/aevilesaguiar", "aeviles@hotmail.com"))
+                .description("Aplicação de Agendamento de Meetups")
+                .version("2.0.0")
+                .contact(new Contact("Aeviles Aguiar", "https://github.com/aevilesaguiar/agenda-service-projeto.git", "aeviles@hotmail.com"))
                 .build();
     }
 
